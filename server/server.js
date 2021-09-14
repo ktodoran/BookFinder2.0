0122b994ef8f3ce8947014fecd838e5079f0fdcd
Kyle Todoran <ktodoran@gmail.com>
2021-09-14 16:24:56 -0400

    Fixed server.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,42 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const express = require('express');
+const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 
-const PORT = process.env.PORT || 3001;
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+// async gunction with server.start(); and applyMiddleware
+async function startApolloServer(typeDefs, resolvers) {
+  await server.start();
+  await server.applyMiddleware({
+    app
+  });
+};
 
-// Define middleware here
+startApolloServer(typeDefs, resolvers);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-// Define API routes here
-app.use(routes)
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
 });
